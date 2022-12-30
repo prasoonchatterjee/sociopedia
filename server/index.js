600f7f31235139c990ee36fc8a8388b64b5ef988
Prasoon Chatterjee <91713113+chatterjeeprasoon@users.noreply.github.com>
2022-12-30 18:14:29 +0530

    signup api setup

--- server/index.js
+++ server/index.js
@@ -9,6 +9,8 @@ import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
+import { register } from './controllers/auth.js';
+
 /* CONFIGURATIONS */
 
 const __filename = fileURLToPath(import.meta.url);
@@ -23,6 +25,20 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
+
+/* FILE STORAGE */
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/assets');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
+
+/* ROUTES WITH FILES */
+app.post('/auth/register', upload.single('picture'), register);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
