0544a353427656ab058577ff992dfa77f2cafc24
Prasoon Chatterjee <91713113+chatterjeeprasoon@users.noreply.github.com>
2022-12-31 16:55:10 +0530

    posts apis added

--- server/index.js
+++ server/index.js
@@ -11,7 +11,8 @@ import { fileURLToPath } from 'url';
 
 import { register } from './controllers/auth.js';
 import authRoutes from './routes/auth.js';
-import userRoutes from "./routes/user.js";
+import userRoutes from './routes/users.js';
+import postRoutes from './routes/posts.js';
 
 /* CONFIGURATIONS */
 
@@ -43,8 +44,9 @@ const upload = multer({ storage });
 app.post('/auth/register', upload.single('picture'), register);
 
 /* ROUTES */
-app.use('/auth', authRoutes)
-app.use("/users", userRoutes);
+app.use('/auth', authRoutes);
+app.use('/users', userRoutes);
+app.use('/posts', postRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
